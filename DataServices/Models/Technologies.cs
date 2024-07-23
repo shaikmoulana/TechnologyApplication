4111812aa35d24d7b91c9046a5927d62ea4c2d16
shaikmoulana <shaikmoulana2002@gmail.com>
2024-07-23 18:26:34 +0530

    Updated Technologies Model class

--- DataServices/Models/Technologies.cs
+++ DataServices/Models/Technologies.cs
@@ -33,5 +33,21 @@ namespace DataServices.Models
         [JsonIgnore] // Add this attribute to ignore during JSON deserialization
         public Departments Departments { get; set; }
     }
+    public class TechnologiesDTO
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The DepartmentId field is required.")]
+        public string DepartmentId { get; set; }
+
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
 
 }
